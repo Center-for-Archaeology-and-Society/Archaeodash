823c6d8c0f99e27f53ca9afdb0f03d44012f8312
Robert Bischoff <bischrob@gmail.com>
2023-09-17 15:28:57 -0700

    added docker

---
+++ inst/runDocker.R
@@ -0,0 +1,10 @@
+# script to build archaeodash docker container
+
+docker build -t archaeodash ./docker
+
+docker run \
+--name=arcaeodash \
+-d \
+--publish=3838:3838 \
+--restart unless-stopped \
+archaeodash
