83b5e070205bfd6a1bde0fc36c1770791ad1a401
Robert Bischoff <bischrob@gmail.com>
2024-05-16 11:46:02 -0700

    updated description

--- R/connect.R
+++ R/connect.R
@@ -1,6 +1,17 @@
-# connect
 
-#@export
+#' Connect
+#'
+#' @param db database name
+#' @param host IP or URL
+#' @param user username
+#' @param pwd  password
+#' @param port port number
+#'
+#' @return database connection
+#' @export
+#'
+#' @examples
+#' connect(db,host,user,pwd,port)
 connect = function(db = Sys.getenv("db"), host = Sys.getenv('host'), user = Sys.getenv('user'), pwd = Sys.getenv('pwd'),port = as.integer(Sys.getenv("port"))){
   con = DBI::dbConnect(RMySQL::MySQL(),
                        host = host,
