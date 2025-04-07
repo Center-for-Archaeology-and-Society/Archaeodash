f07e2f86ff8110227bf2069a10d0f41a7d021f52
Robert Bischoff <bischrob@gmail.com>
2025-04-07 10:10:34 -0700

    removed test

--- R/connect.R
+++ R/connect.R
@@ -12,7 +12,7 @@
 #'
 #' @examples
 #' connect(db,host,user,pwd,port)
-connect = function(db = Sys.getenv("db"), host = Sys.getenv('host'), user = Sys.getenv('user'), pwd = Sys.getenv('pwdasd'),port = as.integer(Sys.getenv("port"))){
+connect = function(db = Sys.getenv("db"), host = Sys.getenv('host'), user = Sys.getenv('user'), pwd = Sys.getenv('pwd'),port = as.integer(Sys.getenv("port"))){
   con = tryCatch({DBI::dbConnect(RMySQL::MySQL(),
                        host = host,
                        dbname = db,
